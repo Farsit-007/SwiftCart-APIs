77698aed3b1de5e528d580d0fa911fc1fd2f2377
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-24 17:18:55 +0600

    user interface added

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -6,3 +6,25 @@ export enum UserRole {
   USER = "user",
 }
 
+// User Schema Definition
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  name: string;
+  role: UserRole;
+  hasShop: boolean;
+  clientInfo: {
+    device: "pc" | "mobile"; // Device type
+    browser: string; // Browser name
+    ipAddress: string; // User IP address
+    pcName?: string; // Optional PC name
+    os?: string; // Optional OS name (Windows, MacOS, etc.)
+    userAgent?: string; // Optional user agent string
+  };
+  lastLogin: Date;
+  isActive: boolean;
+  otpToken?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
