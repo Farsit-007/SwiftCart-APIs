5a164a2ab92f15f5bbde9d33e44f927b8ecf09d7
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-24 17:22:03 +0600

    customer schema added

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -19,3 +19,35 @@ const userValidationSchema = z.object({
     clientInfo: clientInfoSchema, // Nested schema for client info
   }),
 });
+
+const customerInfoValidationSchema = z.object({
+  body: z
+    .object({
+      phoneNo: z
+        .string()
+        .regex(/^\d{11}$/, "Phone number must be exactly 11 digits long")
+        .optional(),
+      gender: z.enum(["Male", "Female", "Other"]).default("Other").optional(),
+      dateOfBirth: z
+        .string()
+        .optional()
+        .refine((value) => !value || !isNaN(Date.parse(value)), {
+          message: "Invalid date format. Must be a valid date.",
+        })
+        .optional(),
+      address: z.string().optional(),
+      photo: z
+        .string()
+        .regex(
+          /^(http(s)?:\/\/.*\.(?:png|jpg|jpeg))$/,
+          "Invalid photo URL format. Must be a valid image URL."
+        )
+        .optional(),
+    })
+    .strict(),
+});
+
+export const UserValidation = {
+  userValidationSchema,
+  customerInfoValidationSchema,
+};
