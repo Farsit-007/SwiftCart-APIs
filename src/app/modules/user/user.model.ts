1e7400be2ff58842b4d25809c507fb42905e9a0b
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-24 17:24:51 +0600

    user model added

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,76 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser, UserModel, UserRole } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
+import AppError from "../../errors/appError";
+import { StatusCodes } from "http-status-codes";
+
+// Create the User schema based on the interface
+const userSchema = new Schema<IUser, UserModel>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: [UserRole.ADMIN, UserRole.USER],
+      default: UserRole.USER,
+    },
+    hasShop: {
+      type: Boolean,
+      default: false, // Default value is false
+    },
+    clientInfo: {
+      device: {
+        type: String,
+        enum: ["pc", "mobile"],
+        required: true,
+      },
+      browser: {
+        type: String,
+        required: true,
+      },
+      ipAddress: {
+        type: String,
+        required: true,
+      },
+      pcName: {
+        type: String,
+      },
+      os: {
+        type: String,
+      },
+      userAgent: {
+        type: String,
+      },
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    otpToken: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
