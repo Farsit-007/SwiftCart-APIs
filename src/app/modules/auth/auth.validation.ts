6c3a591bc7de412383421174b3bacec0bd225533
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-24 17:01:58 +0600

    refeshtoken schema added

--- src/app/modules/auth/auth.validation.ts
+++ src/app/modules/auth/auth.validation.ts
@@ -10,3 +10,12 @@ const loginZodSchema = z.object({
     }),
   }),
 });
+
+const refreshTokenZodSchema = z.object({
+  cookies: z.object({
+    refreshToken: z.string({
+      required_error: "Refresh Token is required",
+    }),
+  }),
+});
+  
