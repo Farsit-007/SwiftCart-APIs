4c994a612a1dea5249e38ed54213a32c495071f5
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-24 17:28:40 +0600

    user get route added

---
+++ src/app/modules/user/user.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { UserController } from "./user.controller";
+import clientInfoParser from "../../middleware/clientInfoParser";
+import validateRequest from "../../middleware/validateRequest";
+import { UserValidation } from "./user.validation";
+import auth from "../../middleware/auth";
+import { UserRole } from "./user.interface";
+import { multerUpload } from "../../config/multer.config";
+import { parseBody } from "../../middleware/bodyParser";
+
+const router = Router();
+
+router.get("/", auth(UserRole.ADMIN), UserController.getAllUser);
+
+
