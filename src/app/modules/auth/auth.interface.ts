66ce89e924c5aad5015c078f27d9e05e06f6bf48
Md Rashedul Islam <rajib5570@gmail.com>
2025-05-24 16:54:29 +0600

    auth interface added

---
+++ src/app/modules/auth/auth.interface.ts
@@ -0,0 +1,13 @@
+import { UserRole } from "../user/user.interface";
+export interface IAuth {
+  email: string;
+  password: string;
+  clientInfo: {
+    device: "pc" | "mobile";
+    browser: string;
+    ipAddress: string;
+    pcName?: string;
+    os?: string;
+    userAgent?: string;
+  };
+}
